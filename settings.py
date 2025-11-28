af23804bd8ae18c39eda2db112b79bba0c5ef32c
elinalakshya <elinalakshya@gmail.com>
2025-11-28 11:46:18 +0530

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+BACKEND_URL = "https://jravis-backend.onrender.com"
+LOCK_CODE = "JRV2040_LOCKED_KEY_001"
+
+# Email settings
+SMTP_HOST = "smtp.gmail.com"
+SMTP_PORT = 587
+EMAIL_USER = "nrveeresh327@gmail.com"
+EMAIL_PASS = "fxyfgqnuswhdgynl"
