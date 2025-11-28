424c0f359447d1a0bf11d5d66b8a8bd61048ea5c
elinalakshya <elinalakshya@gmail.com>
2025-11-28 12:57:43 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,5 +4,5 @@ LOCK_CODE = "JRV2040_LOCKED_KEY_001"
 # Email settings
 SMTP_HOST = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_USER = "nrveeresh327@gmail.com"
+EMAIL_USER = "elinalakshya@gmail.com"
 EMAIL_PASS = "fxyfgqnuswhdgynl"
