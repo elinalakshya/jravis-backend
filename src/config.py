5b498a9c49f593d499dc1fd9fa1704e7da60f79a
elinalakshya <elinalakshya@gmail.com>
2025-12-06 02:09:17 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "JRAVIS Backend"
     VERSION: str = "1.0.0"
 
     OPENAI_API_KEY: str = ""
-    N8N_WEBHOOK_SECRET: str = ""  # NEW FIELD
+    N8N_WEBHOOK_SECRET: str = ""
 
     class Config:
         env_file = ".env"
