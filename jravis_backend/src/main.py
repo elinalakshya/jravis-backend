5cf423146b411ed790d51d89cf5570d27317a0ab
Elina lakshya <46061371-elinalakshya201@users.noreply.replit.com>
2025-12-08 12:48:22 +0000

    Fix import path in main.py

--- jravis_backend/src/main.py
+++ jravis_backend/src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-from jravis-backend.src.settings import settings
+from jravis_backend.src.settings import settings
 
 # Routers
 from router_health import router as health_router
