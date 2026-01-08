5a90665c068c11009192484b62bd07313e5bc83e
elinalakshya <elinalakshya@gmail.com>
2026-01-08 18:53:56 +0530

    Create app.py

---
+++ src/src/app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI(title="JRAVIS Backend", version="1.0")
+
+
+@app.get("/")
+def health():
+    return {
+        "status": "ok",
+        "service": "jravis",
+        "env": os.getenv("ENV", "prod")
+    }
+
+
+@app.get("/ping")
+def ping():
+    return {"message": "JRAVIS is alive 🚀"}
