9769800553470e9ac4ddbd2cb0fa0f2d11c3172e
elinalakshya <elinalakshya@gmail.com>
2026-01-25 18:33:35 +0530

    Update unified_engine.py

--- src/src/unified_engine.py
+++ src/src/unified_engine.py
@@ -1,22 +1,9 @@
-from publishing_engine import run_publishers
-
-
-def run_all_streams_micro_engine(file_path, title, description, price):
+def run_all_streams_micro_engine(product):
     print("🚀 UNIFIED ENGINE STARTED")
-    print("📦 FILE PATH :", file_path)
-    print("🧩 TITLE     :", title)
-    print("💰 PRICE     :", price)
-    print("📤 STARTING PUBLISHING PIPELINE")
+    print("📦 ZIP PATH :", product["zip_path"])
+    print("🧩 TITLE    :", product["title"])
+    print("💰 PRICE    :", product["price"])
 
-    results = run_publishers(
-        file_path=file_path,
-        title=title,
-        description=description,
-        price=price,
-    )
-
-    print("🏁 PUBLISHING FINISHED")
-    print("📊 RESULTS:", results)
-
-    return results
-
+    return {
+        "download_zip": product["zip_path"]
+    }
