a0fbdbb930906f0e7adbe1f71600d3451b5e3474
elinalakshya <elinalakshya@gmail.com>
2026-01-25 18:39:00 +0530

    Update unified_engine.py

--- src/src/unified_engine.py
+++ src/src/unified_engine.py
@@ -2,7 +2,6 @@ def run_all_streams_micro_engine(product):
     print("🚀 UNIFIED ENGINE STARTED")
     print("📦 ZIP PATH :", product["zip_path"])
     print("🧩 TITLE    :", product["title"])
-    print("💰 PRICE    :", product["price"])
 
     return {
         "download_zip": product["zip_path"]
