b9665355414fdcd438d0c24b973e0ae690fccb0b
elinalakshya <elinalakshya@gmail.com>
2026-01-26 20:19:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@
 
 from fastapi import FastAPI
 from api.factory import router as factory_router
+from jravis_backend.services.api.publish import router as publish_router
+
+app.include_router(factory_router)
+app.include_router(publish_router)
+
 
 app = FastAPI(title="JRAVIS Backend")
 
